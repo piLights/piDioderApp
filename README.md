f935c78cc405cfbe8d0a2f8af7f6ca389091fa0c
Jannick Fahlbusch <jannick@jf-projects.de>
2015-06-23 20:21:24 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+PiDioderApp
+---
+
+This is the PiDioder-App.
+
+It allows you, to control your Dioder wirelessly over the internet.
+
+
+Make sure, you fill in your Domain in www/js/dioder.js
---
+++ www/js/dioder.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	var $colors = $('.container div[data-type="color"]'),
+			$picker = $('.picker'),
+			temp = $picker.css('height');
+
+	function getLayout() {
+		temp = $picker.css('height');
+		$('.container').css('padding-bottom', temp);
+	};
+	getLayout();
+
+	$(document).on('click, mousemove', function(){
+		getLayout();
+
+	})
+
+	$colors.each(function(){
+		$(this).html('<div class="flipper"><div class="origin"></div><div class="colors"><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i></div></div>')
+		.addClass('turn180')
+		.find('i').click(function(){
+			var selectColor = $(this).css('backgroundColor'),
+					colorHex = rgb2hex(selectColor);
+					 $.post( "yourURL.TLD", { hex: rgb2hex2(selectColor)});
+					$picker.removeClass('hide');
+
+		})
+	})
+
+	$(document).on('click', '.picker', function(event){
+		$.get( "yourURL.TLD");
+		$picker.toggleClass('hide');
+		getLayout();
+	})
+
+	$(document).on('click', '.code', function(event){
+		$(this).select();
+	})
+
+	function rgb2hex(rgb) {
+		rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+		function hex(x) {
+				return ("0" + parseInt(x).toString(16)).slice(-2);
+		}
+		return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+	}
+	function rgb2hex2(rgb) {
+		rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+		function hex(x) {
+				return ("0" + parseInt(x).toString(16)).slice(-2);
+		}
+		return hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+	}
+
+})
